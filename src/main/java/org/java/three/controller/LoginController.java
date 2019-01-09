198e3b5053a6c0c8d98759343d734d8aa0913cfd
userwangqi <2529440106@qq.com>
2019-01-09 13:59:35 +0800

    1111111111111

---
+++ src/main/java/org/java/three/controller/LoginController.java
@@ -0,0 +1,21 @@
+package org.java.three.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/login")
+    private  String longinGo(){
+        //2222
+        System.out.println("login");
+        return  "/three/login";
+    }
+
+    @RequestMapping("/Goindex")
+    private  String index(){
+        return  "/three/index";
+    }
+
+
+}
